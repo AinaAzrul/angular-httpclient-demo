04cd3fab601fb2f38202f4c5d368201f09493b7a
Aina Diyanah <ainardm22@gmail.com>
2022-04-29 09:51:54 +0800

    commit-1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { Routes, RouterModule } from '@angular/router';
+import { AddIssueComponent } from './components/add-issue/add-issue.component';
+import { EditIssueComponent } from './components/edit-issue/edit-issue.component';
+import { IssueListComponent } from './components/issue-list/issue-list.component';
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'add-issue' },
+  { path: 'add-issue', component: AddIssueComponent },
+  { path: 'edit-issue/:id', component: EditIssueComponent },
+  { path: 'issues-list', component: IssueListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'; //ERROR: coludnt use 'form' world cus not declared in shared module
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddIssueComponent } from './components/add-issue/add-issue.component';
+import { EditIssueComponent } from './components/edit-issue/edit-issue.component';
+import { IssueListComponent } from './components/issue-list/issue-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BugService } from './shared/bug.service'; 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddIssueComponent,
+    EditIssueComponent,
+    IssueListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+
+  exports:[
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [BugService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
